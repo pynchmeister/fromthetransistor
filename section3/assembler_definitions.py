3c38e8215f8ffb8c255487a43e19009c017cdd34
Amnot <acyclicgraphs@gmail.com>
2023-06-24 12:52:08 -0400

    Create assembler_definitions.py

---
+++ section3/assembler_definitions.py
@@ -0,0 +1,14 @@
+# Define the opcode for each instruction
+instruction_opcodes = {
+    'ADD': '0001',
+    'SUB': '0010',
+    'MOV': '0011'
+}
+
+# Define the binary representation for each register
+registers = {
+    'R0': '0000',
+    'R1': '0001',
+    'R2': '0010',
+    'R3': '0011'
+}
